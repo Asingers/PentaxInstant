4c20f9593cd3461034d4e6300b97bc513635cf27
Josh Levinger <josh@levinger.net>
2017-07-17 12:29:51 -0700

    basic photos listing

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,97 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Simple React Native App
+ * to display images from a Pentax camera over WiFi
+ * and save them to the iOS Camera Roll
+ * @jlev 2017
  */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
-  View
+  View,
+  CameraRoll,
+  ActivityIndicator
 } from 'react-native';
 
-export default class test extends Component {
+export default class PentaxInstant extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photosJSON: {},
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPhotosJSON();
+  }
+
+  fetchPhotosJSON() {
+    console.log('fetchPhotosJSON');
+
+    var url = 'http://localhost:8000/v1/photos';
+    fetch(url)
+      .then( response => response.json() )
+      .then( jsonData => {
+        console.log('jsonData', jsonData);
+
+        this.setState({
+          isLoading: false,
+          photosJSON: jsonData.dirs
+        });
+      })
+    .catch( error => console.log('Fetch error ' + error) );
+  }
+
   render() {
+    var {isLoading} = this.state;
+    if(isLoading)
+      return this.renderLoadingMessage();
+    else
+      return this.renderResults();
+  }
+
+  renderLoadingMessage() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.loadingContainer}>
+          <ActivityIndicator
+            animating={true}
+            color={'#fff'}
+            size={'small'} 
+            style={{margin: 15}} />
+            <Text style={{color: '#fff'}}>Contacting Camera</Text>
+      </View>
+    );
+  }
+
+  renderResults() {
+    var {photosJSON, isLoading} = this.state;
+    console.log('photosJSON',photosJSON);
+    return (
+      <View>
+        {photosJSON.map((folder, index) => {
+          return(
+            <Text key={index}>
+              {folder.name}
+            </Text>
+          );
+        })}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loadingContainer: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#000'
+  }
 });
 
-AppRegistry.registerComponent('test', () => test);
+AppRegistry.registerComponent('PentaxInstant', () => PentaxInstant);
